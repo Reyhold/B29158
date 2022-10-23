eac2d0298648d57ea4a84350a50223b62cb0cbf4
nikit <nikitamarincenko5@gmail.com>
2022-10-23 14:27:43 +0300

    Different Choices Commit

--- R P S.py
+++ R P S.py
@@ -7,3 +7,32 @@ def game(choice, result):
     print("--------------------------------")
     print("Your select – ", str.capitalize(choice))
     print("Computer select —", str.capitalize(computer_choice))
+
+    if str.lower(choice) == computer_choice:
+        print("Result of game – Draw")
+        print("Score,Computer", result["computer"], "—", result["player"], "Player")
+
+    elif str.lower(choice) == "r" and computer_choice == "p":
+        result["computer"] += 1
+        print("------Computer Wins------")
+        print("Score,Computer", result["computer"], "—", result["player"], "Player")
+    elif str.lower(choice) == "r" and computer_choice == "s":
+        result["player"] += 1
+        print("------Player Wins------")
+        print("Score,Computer", result["computer"], "—", result["player"], "Player")
+    elif str.lower(choice) == "p" and computer_choice == "s":
+        result["computer"] += 1
+        print("------Computer Wins------")
+        print("Score,Computer", result["computer"], "—", result["player"], "Player")
+    elif str.lower(choice) == "p" and computer_choice == "r":
+        result["player"] += 1
+        print("------Player Wins------")
+        print("Score,Computer", result["computer"], "—", result["player"], "Player")
+    elif str.lower(choice) == "s" and computer_choice == "r":
+        result["computer"] += 1
+        print("------Computer Wins------")
+        print("Score,Computer", result["computer"], "—", result["player"], "Player")
+    elif str.lower(choice) == "s" and computer_choice == "p":
+        result["player"] += 1
+        print("------Player Wins------")
+        print("Score,Computer", result["computer"], "—", result["player"], "Player")
