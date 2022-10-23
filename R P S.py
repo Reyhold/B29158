37115dc4b34ea1cba114f780ba0d0c87360fbee3
nikit <nikitamarincenko5@gmail.com>
2022-10-23 14:22:28 +0300

    Choice of Weapon Commit

--- R P S.py
+++ R P S.py
@@ -3,3 +3,7 @@ import random
 def game(choice, result):
     print("")
     print("=====Start Game rock, paper,  scissors=====")
+    computer_choice = random.choice("rps")
+    print("--------------------------------")
+    print("Your select – ", str.capitalize(choice))
+    print("Computer select —", str.capitalize(computer_choice))
